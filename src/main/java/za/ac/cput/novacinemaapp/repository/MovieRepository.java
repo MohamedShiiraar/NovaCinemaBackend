76cf8ff08eec15aee4de4ce8303f75e054c0b846
MohamedShiiraar <mso2shiiraar@gmail.com>
2024-05-17 21:37:01 +0200

    Added User and Movie Repository

--- src/main/java/za/ac/cput/novacinemaapp/repository/MovieRepository.java
+++ src/main/java/za/ac/cput/novacinemaapp/repository/MovieRepository.java
@@ -1,4 +1,16 @@
 package za.ac.cput.novacinemaapp.repository;
 
-public interface MovieRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.novacinemaapp.domain.Movie;
+
+import java.util.List;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie,String> {
+    List<Movie> findMovieBy(String name);
+    List<Movie> findMoviesByGenre(String genre);
+    List<Movie> findMoviesByAgeRestriction(String ageRestriction);
+    void deleteMovieByName(String name);
+
 }

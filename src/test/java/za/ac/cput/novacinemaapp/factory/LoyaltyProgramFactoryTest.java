5d421220cb0323030c7240dcc5cd8a9725ce9ca5
Gammaadmo <220208344@mycput.ac.za>
2024-05-18 00:19:36 +0200

    Completed Test(loyaltyProgram)

--- src/test/java/za/ac/cput/novacinemaapp/factory/LoyaltyProgramFactoryTest.java
+++ src/test/java/za/ac/cput/novacinemaapp/factory/LoyaltyProgramFactoryTest.java
@@ -1,7 +1,29 @@
 package za.ac.cput.novacinemaapp.factory;
 
+import org.junit.jupiter.api.Test;
+import za.ac.cput.novacinemaapp.domain.LoyaltyProgram;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class LoyaltyProgramFactoryTest {
 
+
+class LoyaltyProgramFactoryTest {
+    @Test
+    void testbuildLoyaltyfactory() {
+
+        LoyaltyProgram l =  LoyaltyProgramFactory.buildLoyaltyProgram("1", "20 points for purchasing a movie ticket", "20","30 December" ,"10","One user per account");
+        assertNotNull(l);
+        System.out.println(l.toString());
+
+
+    }
+    @Test
+    void testbuildLoyaltyfactorywithfail() {
+
+        LoyaltyProgram l =  LoyaltyProgramFactory.buildLoyaltyProgram("", "20 points for purchasing a movie ticket", "20","30 December" ,"10","One user per account");
+        assertNotNull(l);
+        System.out.println(l.toString());
+
+
+    }
 }

dc058c96d2cb9e1d351917cddfa98b7dd75299a2
Jared <jaredbarros904@gmail.com>
2024-05-17 17:13:48 +0200

    Adding domain classes of CardPayment and Cinema

--- src/main/java/za/ac/cput/novacinemaapp/domain/Cinema.java
+++ src/main/java/za/ac/cput/novacinemaapp/domain/Cinema.java
@@ -1,4 +1,71 @@
 package za.ac.cput.novacinemaapp.domain;
 
+import java.util.*;
+
 public class Cinema {
+    private String locationID;
+    private String name;
+
+    public Cinema() {
+    }
+
+    public Cinema(Builder builder) {
+        this.locationID = builder.locationID;
+        this.name = builder.name;
+    }
+
+    public String getLocationID() {
+        return locationID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(!(o instanceof Cinema)) return false;
+        Cinema cinema = (Cinema) o;
+        return Objects.equals(locationID, cinema.locationID) &&
+        Objects.equals(name, cinema.name);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(locationID, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Cinema{" +
+                "locationID='" + locationID + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String locationID;
+        private String name;
+
+        public Builder setLocationID(String locationID) {
+            this.locationID = locationID;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder copy(Cinema cinema) {
+            this.locationID = cinema.locationID;
+            this.name = cinema.name;
+            return this;
+        }
+
+        public Cinema build() {
+            return new Cinema(this);
+        }
+    }
 }

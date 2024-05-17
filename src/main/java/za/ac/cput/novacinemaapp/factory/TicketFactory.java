fa6258ff561d97e6bf8f0698f98416c7500e37ac
amaan23 <amaanallie19@gmail.com>
2024-05-17 21:28:32 +0200

    Update4

--- src/main/java/za/ac/cput/novacinemaapp/factory/TicketFactory.java
+++ src/main/java/za/ac/cput/novacinemaapp/factory/TicketFactory.java
@@ -1,15 +1,18 @@
 package za.ac.cput.novacinemaapp.factory;
 
 import za.ac.cput.novacinemaapp.domain.Ticket;
-import za.ac.cput.util.Helper;
+import za.ac.cput.novacinemaapp.util.Helper;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 
 public class TicketFactory {
     public static Ticket buildTicket(String ticketID, String movie, String theatre, String cinema, double ticketPrice, LocalDate date, LocalTime time) {
-        if (Helper.isNullOrEmpty(ticketID) || Helper.isNullOrEmpty(movie) || Helper.isNullOrEmpty(theatre) || Helper.isNullOrEmpty(cinema) || ticketPrice <= 0 || date == null || time == null)
+        if (Helper.isNullOrEmpty(ticketID) || Helper.isNullOrEmpty(movie) || Helper.isNullOrEmpty(theatre) || Helper.isNullOrEmpty(cinema) || Helper.isNullOrEmpty(ticketPrice) || Helper.isNullOrEmpty(date) || Helper.isNullOrEmpty(time))
             return null;
 
-        return new Ticket.Builder().setTicketID(ticketID)
-                .setMovie(movie)
+        return new Ticket.Builder().setTicketID(ticketID).setMovie(movie)
                 .setTheatre(theatre)
                 .setCinema(cinema)
                 .setTicketPrice(ticketPrice)
@@ -19,13 +22,12 @@ public class TicketFactory {
     }
 
     public static Ticket buildTicket(String movie, String theatre, String cinema, double ticketPrice, LocalDate date, LocalTime time) {
-        if (Helper.isNullOrEmpty(movie) || Helper.isNullOrEmpty(theatre) || Helper.isNullOrEmpty(cinema) || ticketPrice <= 0 || date == null || time == null)
+        if (Helper.isNullOrEmpty(movie) || Helper.isNullOrEmpty(theatre) || Helper.isNullOrEmpty(cinema) || Helper.isNullOrEmpty(ticketPrice) || Helper.isNullOrEmpty(date) || Helper.isNullOrEmpty(time))
             return null;
 
         String ticketID = Helper.generateId();
 
-        return new Ticket.Builder().setTicketID(ticketID)
-                .setMovie(movie)
+        return new Ticket.Builder().setTicketID(ticketID).setMovie(movie)
                 .setTheatre(theatre)
                 .setCinema(cinema)
                 .setTicketPrice(ticketPrice)

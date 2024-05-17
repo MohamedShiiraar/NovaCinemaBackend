2762aa1ba5893aa389a4846b28ce91f552f44218
amaan23 <amaanallie19@gmail.com>
2024-05-17 21:19:30 +0200

    Update3

--- src/main/java/za/ac/cput/novacinemaapp/repository/TicketRepository.java
+++ src/main/java/za/ac/cput/novacinemaapp/repository/TicketRepository.java
@@ -1,4 +1,14 @@
 package za.ac.cput.novacinemaapp.repository;
 
-public interface TicketRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.novacinemaapp.domain.Ticket;
+
+import java.util.List;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, String>{
+    List<Ticket> findTicketByTicketID(String ticketId);
 }
+

250875c697a9bb09065bbd86549fa1b3542868e5
Daany34 <220094934@mycput.ac.za>
2024-05-17 17:35:39 +0200

    Seat Factory completed

--- src/main/java/za/ac/cput/novacinemaapp/factory/SeatFactory.java
+++ src/main/java/za/ac/cput/novacinemaapp/factory/SeatFactory.java
@@ -1,4 +1,15 @@
 package za.ac.cput.novacinemaapp.factory;
 
+import za.ac.cput.novacinemaapp.domain.Seat;
+import za.ac.cput.novacinemaapp.util.Helper;
+
 public class SeatFactory {
+    public static Seat buildSeat(String seatNumber, String seatType){
+        if(Helper.isNullOrEmpty(seatNumber)||
+        Helper.isNullOrEmpty(seatType)){
+            return null;
+        }
+        String seatID = Helper.generateId();
+        return new Seat.Builder().setSeatID(seatID).setSeatNumber(seatNumber).setSeatType(seatType).build();
+    }
 }

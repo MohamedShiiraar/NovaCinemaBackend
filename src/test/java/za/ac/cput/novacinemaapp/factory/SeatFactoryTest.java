d46320ca669f2c7c4763d5cac9fd2901c9e65a71
Daany34 <220094934@mycput.ac.za>
2024-05-17 18:48:45 +0200

    Seat Factory tests and amended seat domain class

--- src/test/java/za/ac/cput/novacinemaapp/factory/SeatFactoryTest.java
+++ src/test/java/za/ac/cput/novacinemaapp/factory/SeatFactoryTest.java
@@ -1,7 +1,27 @@
 package za.ac.cput.novacinemaapp.factory;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import za.ac.cput.novacinemaapp.domain.Seat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class SeatFactoryTest {
+    @Test
+    @Order(1)
+    void testBuildSeat(){
+        Seat seat = SeatFactory.buildSeat("D4","Regular");
+        assertNotNull(seat);
+        System.out.println(seat.toString());
+    }
 
+    @Test
+    @Order(2)
+    void testFail(){
+        Seat seat = SeatFactory.buildSeat("", "Regular");
+        assertNotNull(seat);
+        System.out.println(seat.toString());
+    }
 }

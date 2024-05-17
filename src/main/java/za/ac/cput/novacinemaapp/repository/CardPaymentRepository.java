d0fe9178a911a97657bec30f7096909fd63da344
MohamedShiiraar <mso2shiiraar@gmail.com>
2024-05-17 21:37:18 +0200

    Added Cinema and CardPayment Repository

--- src/main/java/za/ac/cput/novacinemaapp/repository/CardPaymentRepository.java
+++ src/main/java/za/ac/cput/novacinemaapp/repository/CardPaymentRepository.java
@@ -1,4 +1,16 @@
 package za.ac.cput.novacinemaapp.repository;
 
-public interface CardPaymentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.novacinemaapp.domain.CardPayment;
+
+import java.util.List;
+
+@Repository
+public interface CardPaymentRepository extends JpaRepository<CardPayment,String> {
+    List<CardPayment> findCardPaymentByCardNumber(String cardNumber);
+
+    List<CardPayment> findCardPaymentByCardHolder(String cardHolder);
+
+    void deleteCardPaymentByCardNumber(String cardNumber);
 }

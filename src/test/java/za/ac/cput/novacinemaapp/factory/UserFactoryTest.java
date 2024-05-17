c4c972e066e1dd6876692f0aff675b2fba076fe5
MohamedShiiraar <mso2shiiraar@gmail.com>
2024-05-17 20:36:47 +0200

    Added User Factory Test

--- src/test/java/za/ac/cput/novacinemaapp/factory/UserFactoryTest.java
+++ src/test/java/za/ac/cput/novacinemaapp/factory/UserFactoryTest.java
@@ -1,7 +1,23 @@
 package za.ac.cput.novacinemaapp.factory;
 
+import org.junit.jupiter.api.Test;
+import za.ac.cput.novacinemaapp.domain.User;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserFactoryTest {
+
+    @Test
+    void testBuildUser() {
+        User b = UserFactory.buildUser("1","Mohamed","Shiiraar","mso2shiiraar@gmail.com","Test123!");
+        assertNotNull(b);
+        System.out.println(b.toString());
+    }
+    @Test
+    void testBuildUserWithFail() {
+        User c = UserFactory.buildUser("","Mohamed","Shiiraar","mso2shiiraar@gmail.com","Test123!");
+        assertNotNull(c);
+        System.out.println(c.toString());
+    }
 
 }

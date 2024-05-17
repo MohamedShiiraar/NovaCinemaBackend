f019d8c764cffb644dffa137c99be4655d57f436
Daany34 <220094934@mycput.ac.za>
2024-05-17 21:33:14 +0200

    Theatre Factory completed

--- src/main/java/za/ac/cput/novacinemaapp/factory/TheatreFactory.java
+++ src/main/java/za/ac/cput/novacinemaapp/factory/TheatreFactory.java
@@ -1,4 +1,14 @@
 package za.ac.cput.novacinemaapp.factory;
 
+import za.ac.cput.novacinemaapp.domain.Cinema;
+import za.ac.cput.novacinemaapp.domain.Theatre;
+import za.ac.cput.novacinemaapp.util.Helper;
+
 public class TheatreFactory {
+    public static Theatre buildTheatre(String theatreType, Cinema cinema){
+        if(Helper.isNullOrEmpty(theatreType) || Helper.isNullOrEmpty(cinema))
+            return null;
+        String theatreID = Helper.generateId();
+        return new Theatre.Builder().setTheatreID(theatreID).setTheatreType(theatreType).setCinema(cinema).build();
+    }
 }

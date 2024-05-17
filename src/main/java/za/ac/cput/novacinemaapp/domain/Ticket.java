e4e32beda42fd3056f13481d425d1584fada38a2
amaan23 <amaanallie19@gmail.com>
2024-05-17 19:58:42 +0200

    Update1

--- src/main/java/za/ac/cput/novacinemaapp/domain/Ticket.java
+++ src/main/java/za/ac/cput/novacinemaapp/domain/Ticket.java
@@ -3,109 +3,139 @@ package za.ac.cput.novacinemaapp.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Objects;
 
 @Entity
 public class Ticket {
     @Id
     private String ticketID;
-    private String title;
-    private String description;
-    private String assignedTo;
-    private String status;
+    private String movie;
+    private String theatre;
+    private String cinema;
+    private double ticketPrice;
+    private LocalDate date;
+    private LocalTime time;
 
     public Ticket() {
     }
 
     public Ticket(Builder builder) {
         this.ticketID = builder.ticketID;
-        this.title = builder.title;
-        this.description = builder.description;
-        this.assignedTo = builder.assignedTo;
-        this.status = builder.status;
+        this.movie = builder.movie;
+        this.theatre = builder.theatre;
+        this.cinema = builder.cinema;
+        this.ticketPrice = builder.ticketPrice;
+        this.date = builder.date;
+        this.time = builder.time;
     }
 
     public String getTicketID() {
         return ticketID;
     }
 
-    public String getTitle() {
-        return title;
+    public String getMovie() {
+        return movie;
     }
 
-    public String getDescription() {
-        return description;
+    public String getTheatre() {
+        return theatre;
     }
 
-    public String getAssignedTo() {
-        return assignedTo;
+    public String getCinema() {
+        return cinema;
     }
 
-    public String getStatus() {
-        return status;
+    public double getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Ticket ticket)) return false;
-        return Objects.equals(ticketID, ticket.ticketID) && Objects.equals(title, ticket.title) && Objects.equals(description, ticket.description) && Objects.equals(assignedTo, ticket.assignedTo) && Objects.equals(status, ticket.status);
+        return Double.compare(ticket.ticketPrice, ticketPrice) == 0 && Objects.equals(ticketID, ticket.ticketID) && Objects.equals(movie, ticket.movie) && Objects.equals(theatre, ticket.theatre) && Objects.equals(cinema, ticket.cinema) && Objects.equals(date, ticket.date) && Objects.equals(time, ticket.time);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(ticketID, title, description, assignedTo, status);
+        return Objects.hash(ticketID, movie, theatre, cinema, ticketPrice, date, time);
     }
 
     @Override
     public String toString() {
         return "Ticket{" +
                 "ticketID='" + ticketID + '\'' +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", assignedTo='" + assignedTo + '\'' +
-                ", status='" + status + '\'' +
+                ", movie='" + movie + '\'' +
+                ", theatre='" + theatre + '\'' +
+                ", cinema='" + cinema + '\'' +
+                ", ticketPrice=" + ticketPrice +
+                ", date=" + date +
+                ", time=" + time +
                 '}';
     }
 
     public static class Builder {
         private String ticketID;
-        private String title;
-        private String description;
-        private String assignedTo;
-        private String status;
+        private String movie;
+        private String theatre;
+        private String cinema;
+        private double ticketPrice;
+        private LocalDate date;
+        private LocalTime time;
 
         public Builder setTicketID(String ticketID) {
             this.ticketID = ticketID;
             return this;
         }
 
-        public Builder setTitle(String title) {
-            this.title = title;
+        public Builder setMovie(String movie) {
+            this.movie = movie;
             return this;
         }
 
-        public Builder setDescription(String description) {
-            this.description = description;
+        public Builder setTheatre(String theatre) {
+            this.theatre = theatre;
             return this;
         }
 
-        public Builder setAssignedTo(String assignedTo) {
-            this.assignedTo = assignedTo;
+        public Builder setCinema(String cinema) {
+            this.cinema = cinema;
             return this;
         }
 
-        public Builder setStatus(String status) {
-            this.status = status;
+        public Builder setTicketPrice(double ticketPrice) {
+            this.ticketPrice = ticketPrice;
+            return this;
+        }
+
+        public Builder setDate(LocalDate date) {
+            this.date = date;
+            return this;
+        }
+
+        public Builder setTime(LocalTime time) {
+            this.time = time;
             return this;
         }
 
         public Builder copy(Ticket ticket) {
             this.ticketID = ticket.ticketID;
-            this.title = ticket.title;
-            this.description = ticket.description;
-            this.assignedTo = ticket.assignedTo;
-            this.status = ticket.status;
+            this.movie = ticket.movie;
+            this.theatre = ticket.theatre;
+            this.cinema = ticket.cinema;
+            this.ticketPrice = ticket.ticketPrice;
+            this.date = ticket.date;
+            this.time = ticket.time;
             return this;
         }
 

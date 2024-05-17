c6c992ac44f84594929ca56dcfa2d056777b88e5
Daany34 <220094934@mycput.ac.za>
2024-05-17 21:35:44 +0200

    Theatre Factory Test completed

--- src/test/java/za/ac/cput/novacinemaapp/factory/TheatreFactoryTest.java
+++ src/test/java/za/ac/cput/novacinemaapp/factory/TheatreFactoryTest.java
@@ -1,7 +1,23 @@
 package za.ac.cput.novacinemaapp.factory;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import za.ac.cput.novacinemaapp.domain.Cinema;
+import za.ac.cput.novacinemaapp.domain.Theatre;
+import za.ac.cput.novacinemaapp.factory.TheatreFactory;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TheatreFactoryTest {
-
+    @Test
+    @Order(1)
+    void testBuildTheatre(){
+        Cinema cinema = CinemaFactory.buildCinema("L123", "Grand Cinema");
+        Theatre theatre = TheatreFactory.buildTheatre("IMAX", cinema);
+        assertNotNull(theatre);
+        System.out.println(theatre);
+    }
 }

59e4281c7c8770fc713fa4e999f4aed924a6bd68
MohamedShiiraar <mso2shiiraar@gmail.com>
2024-05-19 19:14:52 +0200

    MovieServiceTest and UserServiceTest done

--- src/test/java/za/ac/cput/novacinemaapp/service/MovieServiceTest.java
+++ src/test/java/za/ac/cput/novacinemaapp/service/MovieServiceTest.java
@@ -1,7 +1,69 @@
 package za.ac.cput.novacinemaapp.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import za.ac.cput.novacinemaapp.domain.Movie;
+import za.ac.cput.novacinemaapp.factory.MovieFactory;
 
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MovieServiceTest {
+
+    @Autowired
+    MovieService movieService;
+
+    private static Movie movie1,movie2;
+
+    @Test
+    @Order(1)
+    void setup() {
+        movie1 = MovieFactory.buildMovie("1", "Cars", "After the race at the Piston Cup Championship ends in a three-way tie-breaker, a rookie Lightning McQueen is desperate to make it to the winning position and take over the veteran Strip Weathers.", "animation", "117 mins", "PG-13");
+        assertNotNull(movie1);
+        System.out.println(movie1);
+
+        movie2 = MovieFactory.buildMovie("2", "Transformers: Rise of the Beasts", "Optimus Prime and the Autobots team up with a down on his luck young man, an aspiring historian and with a powerful faction of Transformers known as the Maximals to combat a sinister force from outer space that threatens the Earth and all of mankind.", "Action/Sci-fi", "127 mins", "PG-13");
+        assertNotNull(movie2);
+        System.out.println(movie2);
+    }
+    @Test
+    @Order(2)
+    void create() {
+        Movie create1 = movieService.create(movie1);
+        assertNotNull(create1);
+        System.out.println(create1);
+
+        Movie create2 = movieService.create(movie2);
+        assertNotNull(create2);
+        System.out.println(create2);
+
+    }
+    @Test
+    @Order(3)
+    void update() {
+        Movie updated1 = new Movie.Builder().copy(movie2).setDuration("128 mins").build();
+        Movie updated = movieService.update(updated1);
+        System.out.println(updated);
+
+    }
+
+    @Test
+    @Order(4)
+    void read () {
+        Movie read = movieService.read(movie1.getMovieID());
+        assertNotNull(read);
+        System.out.println(read);
+    }
+
+    @Test
+    @Order(5)
+    void getall() {
+        System.out.println(movieService.getall());
+
+    }
 
 }
--- src/test/java/za/ac/cput/novacinemaapp/service/UserServiceTest.java
+++ src/test/java/za/ac/cput/novacinemaapp/service/UserServiceTest.java
@@ -1,7 +1,71 @@
 package za.ac.cput.novacinemaapp.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import za.ac.cput.novacinemaapp.domain.User;
+import za.ac.cput.novacinemaapp.factory.UserFactory;
 
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    private static User user1,user2;
+
+    @Test
+    @Order(1)
+
+    void setup() {
+        user1 = UserFactory.buildUser("1","Mohamed","Shiiraar","mso2shiiraar@gmail.com","Test123!");
+        assertNotNull(user1);
+        System.out.println(user1);
+
+        user2 = UserFactory.buildUser("2","Daanyaal","Isaacs","daanyaalisaacs@gmail.com","Test123!");
+        assertNotNull(user2);
+        System.out.println(user2);
+
+    }
+
+    @Test
+    @Order(2)
+    void create() {
+        User create1 = userService.create(user1);
+        assertNotNull(create1);
+        System.out.println(create1);
+
+        User create2 = userService.create(user2);
+        assertNotNull(create2);
+        System.out.println(create2);
+    }
+
+    @Test
+    @Order(3)
+    void update () {
+        User updated1 = new User.Builder().copy(user2).setName("Jared").setSurname("Barros").setEmailAddress("jared@gmail.com").build();
+        User updated = userService.update(updated1);
+        System.out.println(updated);
+
+    }
+
+    @Test
+    @Order(4)
+    void read() {
+        User read = userService.read(user1.getUserID());
+        assertNotNull(read);
+        System.out.println(read);
+    }
+
+    @Test
+    @Order(5)
+    void getall () {
+        System.out.println(userService.getall());
+    }
 
 }

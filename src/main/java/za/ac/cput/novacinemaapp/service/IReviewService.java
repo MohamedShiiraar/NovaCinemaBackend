48dc3efc795edfa67b8975c30a7675847eb5e12c
youngcoder23 <41047190+youngcoder23@users.noreply.github.com>
2024-05-19 19:11:15 +0200

    IService and IRequest
    Declared my IService classes for Showtime and Review

--- src/main/java/za/ac/cput/novacinemaapp/service/IReviewService.java
+++ src/main/java/za/ac/cput/novacinemaapp/service/IReviewService.java
@@ -1,4 +1,14 @@
 package za.ac.cput.novacinemaapp.service;
+import  za.ac.cput.novacinemaapp.domain.Review;
+import java.util.List;
 
-public interface IReviewService {
+/*  IReviewService.java
+ *   Service Interface for Review
+ *   Author: Musaddiq McWhite (219369151)
+ *   19 May 2024
+ * */
+
+public interface IReviewService extends IService<Review, String> {
+
+    List<Review> getall();
 }
--- src/main/java/za/ac/cput/novacinemaapp/service/IShowtimeService.java
+++ src/main/java/za/ac/cput/novacinemaapp/service/IShowtimeService.java
@@ -1,4 +1,14 @@
 package za.ac.cput.novacinemaapp.service;
+import za.ac.cput.novacinemaapp.domain.Showtime;
+import java.util.List;
 
-public interface IShowtimeService {
+/*  IShowtimeService.java
+ *   Service Interface for Showtime
+ *   Author: Musaddiq McWhite (219369151)
+ *   19 May 2024
+ * */
+
+public interface IShowtimeService extends IService<Showtime, String> {
+    List<Showtime> getall();
+
 }

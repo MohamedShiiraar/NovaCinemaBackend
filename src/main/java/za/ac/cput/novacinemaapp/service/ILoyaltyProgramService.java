dd3a169c9247e1446a95cf7f42270fa3a45640c8
Gammaadmo <220208344@mycput.ac.za>
2024-05-19 15:40:09 +0200

    Completed ILoyaltyProgramService(loyaltyProgram)

--- src/main/java/za/ac/cput/novacinemaapp/service/ILoyaltyProgramService.java
+++ src/main/java/za/ac/cput/novacinemaapp/service/ILoyaltyProgramService.java
@@ -1,4 +1,12 @@
 package za.ac.cput.novacinemaapp.service;
 
-public interface ILoyaltyProgramService {
+import za.ac.cput.novacinemaapp.domain.LoyaltyProgram;
+
+import java.util.Set;
+
+public interface  ILoyaltyProgramService extends IService<LoyaltyProgram, String> {
+    Set<LoyaltyProgram> getall();
+
+
+
 }

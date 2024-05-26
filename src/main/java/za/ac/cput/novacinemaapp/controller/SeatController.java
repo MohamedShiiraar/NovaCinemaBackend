cfe63893eb294e5716d063063a3caad14b440a93
Daany34 <220094934@mycput.ac.za>
2024-05-26 12:58:18 +0200

    Finished Seat Controller

---
+++ src/main/java/za/ac/cput/novacinemaapp/controller/SeatController.java
@@ -0,0 +1,49 @@
+package za.ac.cput.novacinemaapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.novacinemaapp.domain.Seat;
+import za.ac.cput.novacinemaapp.service.SeatService;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/seat")
+public class SeatController {
+    @Autowired
+    SeatService service;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody Seat seat){
+        Seat createdSeat = service.create(seat);
+        if(createdSeat==null){
+            ResponseEntity.badRequest().body("Error Creating seat.");
+        }
+        return new ResponseEntity<>(createdSeat, HttpStatus.CREATED);
+    }
+
+    @PostMapping("read/{seatID")
+    public ResponseEntity<?> get(@PathVariable String seatID){
+        Seat seat = service.read(seatID);
+        if(seat==null){
+            ResponseEntity.badRequest().body("Seat with SeatID: "+seatID+" does not exist");
+        }
+        return ResponseEntity.ok(seat);
+    }
+
+    @PostMapping("/getAll")
+    public Set<Seat> getAll(){
+        return service.getAll();
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<?> update(@RequestBody Seat seat){
+        Seat updatedSeat = service.update(seat);
+        if(updatedSeat==null){
+            return ResponseEntity.badRequest().body("Error Updating Seat");
+        }
+        return ResponseEntity.ok(updatedSeat);
+    }
+}

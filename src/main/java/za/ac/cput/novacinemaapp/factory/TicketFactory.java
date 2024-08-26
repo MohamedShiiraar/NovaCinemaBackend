f34bb602323c97b36ec4b1cef8ce4e50d7a1d1ad
amaan23 <amaanallie19@gmail.com>
2024-08-26 10:15:56 +0200

    Updated classes for Ticket

--- src/main/java/za/ac/cput/novacinemaapp/factory/TicketFactory.java
+++ src/main/java/za/ac/cput/novacinemaapp/factory/TicketFactory.java
@@ -1,43 +1,43 @@
 package za.ac.cput.novacinemaapp.factory;
 
-
-// Entity for Ticket
+// Factory for Ticket
 // Author Amaan Allie
 // 17 May 2024
-
 
 import za.ac.cput.novacinemaapp.domain.Ticket;
 import za.ac.cput.novacinemaapp.util.Helper;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class TicketFactory {
-    public static Ticket buildTicket(String ticketID, String movie, String theatre, String cinema, double ticketPrice, LocalDate date, LocalTime time) {
-        if (Helper.isNullOrEmpty(ticketID) || Helper.isNullOrEmpty(movie) || Helper.isNullOrEmpty(theatre) || Helper.isNullOrEmpty(cinema) || Helper.isNullOrEmpty(ticketPrice) || Helper.isNullOrEmpty(date) || Helper.isNullOrEmpty(time))
+
+    public static Ticket buildTicket(String ticketID, String movie, LocalTime showtime, String seat, String cinema, double ticketPrice) {
+        if (Helper.isNullOrEmpty(ticketID) || Helper.isNullOrEmpty(movie) || Helper.isNullOrEmpty(showtime) || Helper.isNullOrEmpty(seat) || Helper.isNullOrEmpty(cinema) || Helper.isNullOrEmpty(ticketPrice))
             return null;
 
-        return new Ticket.Builder().setTicketID(ticketID).setMovie(movie)
-                .setTheatre(theatre)
+        return new Ticket.Builder()
+                .setTicketID(ticketID)
+                .setMovie(movie)
+                .setShowtime(showtime)
+                .setSeat(seat)
                 .setCinema(cinema)
                 .setTicketPrice(ticketPrice)
-                .setDate(date)
-                .setTime(time)
                 .build();
     }
 
-    public static Ticket buildTicket(String movie, String theatre, String cinema, double ticketPrice, LocalDate date, LocalTime time) {
-        if (Helper.isNullOrEmpty(movie) || Helper.isNullOrEmpty(theatre) || Helper.isNullOrEmpty(cinema) || Helper.isNullOrEmpty(ticketPrice) || Helper.isNullOrEmpty(date) || Helper.isNullOrEmpty(time))
+    public static Ticket buildTicket(String movie, LocalTime showtime, String seat, String cinema, double ticketPrice) {
+        if (Helper.isNullOrEmpty(movie) || Helper.isNullOrEmpty(showtime) || Helper.isNullOrEmpty(seat) || Helper.isNullOrEmpty(cinema) || Helper.isNullOrEmpty(ticketPrice))
             return null;
 
         String ticketID = Helper.generateId();
 
-        return new Ticket.Builder().setTicketID(ticketID).setMovie(movie)
-                .setTheatre(theatre)
+        return new Ticket.Builder()
+                .setTicketID(ticketID)
+                .setMovie(movie)
+                .setShowtime(showtime)
+                .setSeat(seat)
                 .setCinema(cinema)
                 .setTicketPrice(ticketPrice)
-                .setDate(date)
-                .setTime(time)
                 .build();
     }
 }
